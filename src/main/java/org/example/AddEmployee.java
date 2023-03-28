691f8628790c95e29e4fefa30aa28502afe676ac
nikitastrigunkov <demonkurt1995@gmail.com>
2023-03-28 21:41:56 +0300

    1

---
+++ src/main/java/org/example/AddEmployee.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddEmployee implements EmployeeDAO {
+    private final String user = "postgres";
+    private final String password = "88635005";
+    private final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+    @Override
+    public List<Employee> getAddEmployee() {
+        List<Employee> employee = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+
+            Statement statement = conn.createStatement();
+            int rows = statement.executeUpdate("INSERT INTO employee (first_name, last_name, gender, age) VALUES ('Sergey', 'Ivanov', 'Male', 50);");
+            System.out.printf("Added %d rows", rows);
+        } catch (Exception ex) {
+            System.out.println("Connection failed...");
+
+
+        }return employee;
+    }
+}
+
+
---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.sql.*;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args)   {
+        EmployeeDAO employeeDAO = new AddEmployee();
+        List <Employee> employees = employeeDAO.getAddEmployee();
+        }
+    }
+
