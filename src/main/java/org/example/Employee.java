f918a1644e6244e9b624adbcc1ef2e13e11f1d4a
nikitastrigunkov <demonkurt1995@gmail.com>
2023-04-05 18:32:07 +0300

    Hiber

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -3,27 +3,38 @@ import javax.persistence.*;
 @Entity
 @Table(name = "employee")
 public class Employee {
-    @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
    private int id;
-    @Column(name = "first_name")
+
    private String first_name;
-    @Column(name = "last_name")
+
    private String last_name;
-    @Column(name = "gender")
-   private String gender;
-    @Column(name = "age")
+
+    private String gender;
+
    private int age;
-    @Column(nullable=true, name = "city_id")
    private int city_id;
 
-    public Employee(String first_name, String last_name, String gender, int age, int city_id) {
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @ManyToOne
+   @JoinColumn(name = "city_id", insertable = false, updatable = false)
+   private City city;
+
+    public Employee(String first_name, String last_name, String gender, int age, City city) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.gender = gender;
         this.age = age;
-        this.city_id = city_id;
-
+        this.city = city;
     }
 
     public Employee() {
@@ -74,17 +85,6 @@ public class Employee {
         return city_id;
     }
 
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", first_name='" + first_name + '\'' +
-                ", last_name='" + last_name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", age=" + age +
-                ", city_id=" + city_id +
-                '}';
-    }
 
     public void setCity_id(int city_id) {
         this.city_id = city_id;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,26 @@ package org.example;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myPersistenceUnit");
 
+        // Создаем экземпляр EntityManager из EntityManagerFactory
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        // Начинаем транзакцию
+        entityManager.getTransaction().begin();
+        City city = new City("fora");
+        entityManager.persist(city);
+        Stream.of(new Employee("Vasy","vasy","M",34, city),
+                new Employee("Vasy2","vasy2","M",35, city))
+                        .peek(employee -> employee.setCity_id(city.getid()))
+                                .forEach(entityManager::persist);
+        entityManager.persist(city);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
